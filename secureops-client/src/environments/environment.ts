53c9fc6e831b078b1f606a67c9628ddd8fe8d95e
kmalate <khalid.malate@gmail.com>
2026-06-02 10:35:45 -0400

    updated apiUrl for client app

--- secureops-client/src/environments/environment.ts
+++ secureops-client/src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: false,
-    apiUrl : "https://localhost:7030",
+    apiUrl : "https://secureops-api-eqcah5bbggcxgnc3.eastus-01.azurewebsites.net/",
     authTokenExpirationTime: 3600, // Token expiration time in seconds (1 hour)
 };
